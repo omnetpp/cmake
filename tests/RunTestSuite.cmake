7a825fc3c19c0245504dea3041c2720f392d2a62
Raphael Riebl <raphael@riebl.net>
2021-07-23 23:07:45 +0200

    tests: add generate_run_script test

--- tests/RunTestSuite.cmake
+++ tests/RunTestSuite.cmake
@@ -1,3 +1,5 @@
+cmake_minimum_required(VERSION 3.13)
+
 if(NOT TEST_SUITE)
     message(FATAL_ERROR "run_test: missing TEST_SUITE parameter")
 endif()
@@ -5,8 +7,12 @@ if(NOT BUILD_DIRECTORY)
     message(FATAL_ERROR "run_test: missing BUILD_DIRECTORY parameter")
 endif()
 
-execute_process(COMMAND ${CMAKE_COMMAND} -S ${TEST_SUITE} -B ${BUILD_DIRECTORY}/${TEST_SUITE}
+set(TEST_SUITE_BINARY_DIR ${BUILD_DIRECTORY}/${TEST_SUITE})
+execute_process(COMMAND ${CMAKE_COMMAND} -S ${TEST_SUITE} -B ${TEST_SUITE_BINARY_DIR}
     RESULT_VARIABLE setup_test_suite)
 if(setup_test_suite)
     message(FATAL_ERROR "run_test: setting up test suite failed")
+elseif(EXISTS "${TEST_SUITE}/post-tests.cmake")
+    include(TestSuiteHelpers.cmake)
+    include(${TEST_SUITE}/post-tests.cmake)
 endif()
