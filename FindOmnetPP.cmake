ce879ddab6a405b665f186f2b47914c31078ebca
Raphael Riebl <raphael@riebl.net>
2021-08-10 10:27:27 +0200

    docs: set up initial Sphinx configuration

--- FindOmnetPP.cmake
+++ FindOmnetPP.cmake
@@ -1,3 +1,83 @@
+#[==[.rst:
+FindOmnetPP
+-----------
+
+This module finds the OMNeT++ libraries and include directory.
+Simply use it by invoking :command:`find_package`:
+
+.. code-block:: cmake
+
+    find_package(OmnetPP
+        [version] [EXACT]   # minimum or EXACT version, e.g. 5.6.2
+        [REQUIRED]          # fail with error if OMNeT++ is not found
+    )
+
+Result variables
+^^^^^^^^^^^^^^^^
+
+``OMNETPP_FOUND``
+  True if OMNeT++ is found.
+
+``OMNETPP_LIBRARIES``
+  List of OMNeT++ library targets, see imported targets below.
+
+``OMNETPP_VERSION``
+  OMNeT++ version number in ``X.Y.Z`` format.
+
+Cache variables
+^^^^^^^^^^^^^^^
+
+``OMNETPP_ROOT``
+  Root directory of OMNeT++.
+
+``OMNETPP_INCLUDE_DIR``
+  Directory containing OMNeT++ header files, usually ``OMNETPP_ROOT``/include.
+
+``OMNETPP_MSGC``
+  The OMNeT++ message compiler.
+
+``OMNETPP_FEATURETOOL``
+  The OMNeT++ feature tool.
+
+``OMNETPP_RUN``
+  The *opp_run* executable for release runs.
+
+``OMNETPP_RUN_DEBUG``
+  The *opp_run_dbg* executable for debug runs.
+
+``OMNETPP_RUNALL``
+  The *opp_runall* script.
+
+``OMNETPP_<X>_LIBRARY_DEBUG`` and ``OMNETPP_<X>_LIBRARY_RELEASE``
+  The debug and release build variants of each OMNeT++ library.
+
+Imported targets
+^^^^^^^^^^^^^^^^
+
+The following targets referring to individual OMNeT++ libraries are imported:
+
+``OmnetPP::<X>``
+  where ``<X>`` refers to one of
+
+  - cmdenv
+  - common
+  - envir
+  - eventlog
+  - layout
+  - main
+  - nedxml
+  - qtenv
+  - qtenv-osg
+  - scave
+  - sim
+  - tkenv
+
+``OmnetPP::header``
+  is an interface target which provides you the compile definitions and include directories
+  for OMNeT++ but no actual library.
+
+#]==]
+
 find_path(OMNETPP_ROOT NAMES bin/omnetpp PATHS ENV PATH PATH_SUFFIXES .. DOC "Path to OMNeT++ root directory")
 find_path(OMNETPP_INCLUDE_DIR NAMES omnetpp.h PATHS ${OMNETPP_ROOT}/include DOC "OMNeT++ include directory")
 find_program(OMNETPP_MSGC NAMES opp_msgtool PATHS ${OMNETPP_ROOT}/bin DOC "OMNeT++ message compiler")
